898a3353beadecfcbcf14631ba3cfe42d198103b
DestineyS-eng <destineysyvster@gmail.com>
2026-08-03 00:53:39 +0100

    Refactor domain_name function for clarity
    code wars task:
    
    Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
    
    * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
    * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
    * url = "https://www.cnet.com"                -> domain name = cnet"

--- extracting domain name.py
+++ extracting domain name.py
@@ -1,18 +1,13 @@
 def domain_name(url):
-    clean=url.replace("https://","").replace("http://","").replace("www.","").replace("/","").replace(":","").replace("http","").replace("https,"")    
-    #removes everything common in a url
-    result=""   
-    # this is a place holder for the domain name 
-    for i in range (len(clean)):
-        if clean[i]==".":
-            result = clean.split('.')[0]  
-            #removes anything after the dot as the domain will be the frist element.
-        else:
-            pass   
-            #passes anything that isnt a dot
-        
-    return result
-        
+    #where the domain will be kept
+    domain=""
+    #removes anything that comes before the domain name 
+    url=url.replace("http://","").replace("https","").replace("http","").replace(":","").replace("//","").replace("/","").replace("www.","").replace("https://","")
+    #will look through the list if there is a dot it will split it via the do and take the frist item
+    for i in range (len(url)):
+        if url[i]==".":
+            domain=url.split(".")[0]
+    return domain
         
         
 x= domain_name("https://www.programiz.com/python-programming/online-compiler/")
