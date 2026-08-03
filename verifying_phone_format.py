c0aa9333db9703bb1da01642f5b3373a0ca913c6
DestineyS-eng <destineysyvster@gmail.com>
2026-08-03 03:25:12 +0100

    Fix comments for consistency in verifying_phone_format.py
    code wars task:
    
    a function that accepts a string, and returns true if it is in the form of a phone number.
    Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
    
    Only worry about the following format:
    (123) 456-7890 (don't forget the space after the close parentheses)
    
    Examples:
    
    "(123) 456-7890"  => true
    "(1111)555 2345"  => false
    "(098) 123 4567"  => false

--- verifying_phone_format.py
+++ verifying_phone_format.py
@@ -3,16 +3,16 @@ def valid_phone_number(phone_number):
     phone=phone_number.split()
     #if the length of the list has 2 elements 
     if len(phone)==2:
-        #if anywhere out side mandatory punctuation is a interger
+        #if anywhere outside mandatory punctuation is a integer
         if phone[0][1:4].isdigit() and phone[1][0:3].isdigit() and phone[1][4:].isdigit():
-            #if the length of the frist element is 5  and brackets in the correct position 
+            #if the length of the first element is 5  and brackets in the correct position 
             if len(phone[0])==5 and phone_number[0]=="(" and phone_number[4]==")":
                 #if there is a space in this area only 
                 if phone_number[5]==" ":
-                    #if the length if the second element is 8 and the hypen is in position 9
+                    #if the length of the second element is 8 and the hyphen is in position 9
                     if len(phone[1])==8 and phone_number[9]=="-":
-                        #once all these conditions are true return true
+                        #once all these conditions are True return True
                         return True
                     
-    #if any arent true false will be returned       
+    #if any arent True False will be returned       
     return False
