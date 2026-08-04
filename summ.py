da8e254fa840cc321287d1a29a80a8795beac7ca
DestineyS-eng <destineysyvster@gmail.com>
2026-08-04 04:56:03 +0100

    Add summation function in summ.py
    code wars task:
    
    Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
    
    For example (Input -> Output):
    
    2 -> 3 (1 + 2)
    8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)

---
+++ summ.py
@@ -0,0 +1,10 @@
+def summation(num):
+    #store the sum of numbers
+    total=0
+    #this will go through the numbers from 0 to whats in num
+    for i in range (0,num):
+        # i is the number thats presented so i+1 will be the number as it starts at 0 
+        total=total+(i+1)
+    return total
+        
+    
