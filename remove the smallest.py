58cea2be3ac5572a9c2846cbfa5521b2d0ea428d
DestineyS-eng <destineysyvster@gmail.com>
2026-08-26 23:12:19 +0100

    Add function to remove the smallest number from list
    code wars task:
    The museum of incredibly dull things
    The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
    
    However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
    
    Task
    Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
    
    Don't change the order of the elements that are left.
    
    Examples
    * Input: [1,2,3,4,5], output = [2,3,4,5]
    * Input: [5,3,2,1,4], output = [5,3,2,4]
    * Input: [2,2,1,2,1], output = [2,2,2,1]

---
+++ remove the smallest.py
@@ -0,0 +1,23 @@
+def remove_smallest(numbers):
+    #if its empty return brackets
+    if numbers==[]:
+        return []
+    #holds the new list excluding the most boring place
+    new=[]
+    #sorted the list and took the frist element to get the smallest number 
+    num=sorted(numbers)
+    num=num[0]
+    #in this for loop it will first check if the current elsement is greater than num
+    #if so ir will append it into new list
+    for i in range (len(numbers)):
+        if numbers[i]>num:
+            new.append(numbers[i])
+        #if not it will then check if the element is equal to num
+        #if there is num before this element
+        #if the overall count of num is greater than 1
+        elif numbers[i]==num and numbers[:i] and numbers.count(num)>1:
+            new.append(numbers[i])
+        #this will result in the frist smallest number being excluded leaving the rest
+    return new
+                
+        
